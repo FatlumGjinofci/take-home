b52113f979454ff153b8396b032c6bb49fbc8ff8
FatlumGjinofci <fgjinofci@gmail.com>
2023-07-19 22:44:11 +0200

    working on get comment doing all the stuff in index method, need to refactor it to be more readable

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -11,14 +11,43 @@ class CommentController extends Controller
 {
     public function index(Request $request)
     {
-        $comments = Comment::get();
+        $comments = Comment::query();
 
 
         $res = $comments->paginate(10);
 
-        if ($request->has('limit')) {
-            $res =$comments->paginate($request->limit);
+        if ($request->has('id')) {
+            $res = $comments->where('id', $request->id);
         }
+        if ($request->has('post_id')) {
+            $res = $comments->where('post_id', $request->post_id);
+        }
+        if ($request->has('content')) {
+            $res = $comments->where('content', 'like', "%".$request->content."%");
+        }
+        if ($request->has('abbreviation')) {
+            $res = $comments->where('abbreviation', 'like', "%".$request->abbreviation);
+        }
+        if($request->has('created_at')) {
+            $res = $comments->whereDate('created_at', $request->created_at);
+        }
+        if ($request->has('updated_at')) {
+            $res = $comments->whereDate('updated_at', $res->updated_at);
+        }
+
+        if ($request->has('sort') && $request->has('direction')) {
+            $res = $comments->orderBy($request->sort, $request->direction);
+        }
+
+        if ($request->has('sort')) {
+            $res = $comments->orderByDesc($request->sort);
+        }
+
+        if ($request->has('with')) {
+            $res = $comments->with('post');
+        }
+
+        $res = $comments->paginate($request->limit ?? 10);
 
         return response()->json([
             'result' => $res,
