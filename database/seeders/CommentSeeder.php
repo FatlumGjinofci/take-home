9e707411b301f13d8be183ac2e269286c439dbcf
FatlumGjinofci <fgjinofci@gmail.com>
2023-07-21 10:45:58 +0200

    wip.. on comment seeder

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Helpers\CommentHelper;
 use App\Models\Comment;
 use App\Models\Post;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class CommentSeeder extends Seeder
@@ -14,7 +15,7 @@ class CommentSeeder extends Seeder
         if (Comment::count() > 1) {
             Comment::truncate();
         }
-        
+
         $dataset = collect(config('data.dataset '));
 
         $dataset->chunk(1000)->each(function ($chunk) {
@@ -26,6 +27,8 @@ class CommentSeeder extends Seeder
                     'post_id' => Post::inRandomOrder()->first()->id,
                     'content' => $item,
                     'abbreviation' => $abbreviation,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
                 ];
             }
 
